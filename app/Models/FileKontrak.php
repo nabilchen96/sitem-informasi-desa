380e8954b611fc9f0ae1ef3d78ae2e46aedf9962
wahidalqorni <idunalqorni@gmail.com>
2023-10-12 20:12:32 +0700

    update migration

--- app/Models/FileKontrak.php
+++ app/Models/FileKontrak.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UsulanProposal extends Model
+class FileKontrak extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+    
 }
--- app/Models/UsulanSuratPenelitian.php
+++ app/Models/UsulanSuratPenelitian.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UsulanProposal extends Model
+class UsulanSuratPenelitian extends Model
 {
     use HasFactory;
 
