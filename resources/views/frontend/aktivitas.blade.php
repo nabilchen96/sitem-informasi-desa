2d5ba815a5b0bcf83dd7e55bfda65b74ae093175
wahidalqorni <idunalqorni@gmail.com>
2023-10-12 16:08:26 +0700

    update time line front

--- resources/views/frontend/aktivitas.blade.php
+++ resources/views/frontend/aktivitas.blade.php
@@ -10,74 +10,86 @@
                     </h6>
                 </div>
                 <br><br>
-                
+
                 <ul class="timeline">
-                    <li @if(@getJadwalAktif()->tahap_ke == 1) style="background: red;" data-year="We Are Here!" @endif  data-text="1. Pengajuan Judul"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 2) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="2. Pengumuman Hasil Seleksi"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 3) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="3. Proposal"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 4) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="4. Seminar Proposal"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 5) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="5. RAB"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 6) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="6. Surat Izin Penelitian"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 7) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="7. Seminar Antara"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 8) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="8. Luaran Penelitian"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 9) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="9. Seminar Hasil"></li>
-                    <li @if(@getJadwalAktif()->tahap_ke == 10) style="background: red;" data-year="We Are Here!" @endif data-year="" data-text="10. Survey Kepuasan Layanan"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 1) style="background: red;" data-year="We Are Here!" @endif
+                        data-text="1. Pengajuan Judul"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 2) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="2. Pengumuman Hasil Seleksi"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 3) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="3. Proposal"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 4) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="4. Seminar Proposal"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 5) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="5. RAB"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 6) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="6. Surat Izin Penelitian"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 7) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="7. Seminar Antara"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 8) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="8. Luaran Penelitian"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 9) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="9. Seminar Hasil"></li>
+                    <li @if (@getJadwalAktif()->tahap_ke == 10) style="background: red;" data-year="We Are Here!" @endif
+                        data-year="" data-text="10. Survey Kepuasan Layanan"></li>
                 </ul>
                 <br><br>
             </section>
-
-            <div class="card mt-5">
-                <div style="width: 100%;">
-                    <form>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <label class="form-label">Program Studi <sup class="text-danger">*</sup></label>
-                                    <select name="" class="form-control border" id="">
-                                        <option value="">DIV TRBU</option>
-                                        <option value="">DIII PPKP</option>
-                                        <option value="">DIII MBU</option>
-                                    </select>
+            @if (@getJadwalAktif()->tahap_ke == 1)
+                <div class="card mt-5">
+                    <div style="width: 100%;">
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="mb-4">
+                                        <label class="form-label">Program Studi <sup class="text-danger">*</sup></label>
+                                        <select name="" class="form-control border" id="">
+                                            <option value="">DIV TRBU</option>
+                                            <option value="">DIII PPKP</option>
+                                            <option value="">DIII MBU</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-4">
+                                        <label class="form-label">Judul Penelitian <sup class="text-danger">*</sup></label>
+                                        <input type="text" class="form-control border" placeholder="Judul Penelitian">
+                                    </div>
+                                    <div class="mb-4">
+                                        <label class="form-label">Jenis Penelitian <sup class="text-danger">*</sup></label>
+                                        <select name="" class="form-control border" id="">
+                                            <option value="">Terapan</option>
+                                            <option value="">Basic</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-4">
+                                        <button class="btn btn-primary">Submit</button>
+                                    </div>
                                 </div>
-                                <div class="mb-4">
-                                    <label class="form-label">Judul Penelitian <sup class="text-danger">*</sup></label>
-                                    <input type="text" class="form-control border" placeholder="Judul Penelitian">
-                                </div>
-                                <div class="mb-4">
-                                    <label class="form-label">Jenis Penelitian <sup class="text-danger">*</sup></label>
-                                    <select name="" class="form-control border" id="">
-                                        <option value="">Terapan</option>
-                                        <option value="">Basic</option>
-                                    </select>
-                                </div>
-                                <div class="mb-4">
-                                    <button class="btn btn-primary">Submit</button>
+                                <div class="col-lg-6">
+                                    <div class="mb-4">
+                                        <label class="form-label">Sub Topik Penelitian <sup
+                                                class="text-danger">*</sup></label>
+                                        <select name="" class="form-control border" id="">
+                                            <option value="">Aviation of Learning Technology</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-4">
+                                        <label class="form-label">Pelaksanaan Penelitian <sup
+                                                class="text-danger">*</sup></label>
+                                        <select name="" class="form-control border" id="">
+                                            <option value="">Kelompok</option>
+                                            <option value="">Mandiri</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-4">
+                                        <label class="form-label">Nama Peneliti <sup class="text-danger">*</sup></label>
+                                        <input type="text" class="form-control border" placeholder="Nama Peneliti">
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-lg-6">
-                                <div class="mb-4">
-                                    <label class="form-label">Sub Topik Penelitian <sup class="text-danger">*</sup></label>
-                                    <select name="" class="form-control border" id="">
-                                        <option value="">Aviation of Learning Technology</option>
-                                    </select>
-                                </div>
-                                <div class="mb-4">
-                                    <label class="form-label">Pelaksanaan Penelitian <sup
-                                            class="text-danger">*</sup></label>
-                                    <select name="" class="form-control border" id="">
-                                        <option value="">Kelompok</option>
-                                        <option value="">Mandiri</option>
-                                    </select>
-                                </div>
-                                <div class="mb-4">
-                                    <label class="form-label">Nama Peneliti <sup class="text-danger">*</sup></label>
-                                    <input type="text" class="form-control border" placeholder="Nama Peneliti">
-                                </div>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 @endsection
