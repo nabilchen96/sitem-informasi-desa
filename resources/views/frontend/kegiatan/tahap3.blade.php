6b3c6dd38a08546d1dd6d34b3c86bcbfe95948f1
M. Wahid Alqorni <alqorni@Ms-MacBook-Pro.local>
2024-03-26 11:40:22 +0700

    tampilkan tgl jadwal di tampilan kegiatan

--- resources/views/frontend/kegiatan/tahap3.blade.php
+++ resources/views/frontend/kegiatan/tahap3.blade.php
@@ -1,5 +1,19 @@
 <div class="card">
     <div style="width: 100%;">
+        <fieldset class="form-group border p-3">
+            <legend class="w-auto px-2">Jadwal Pengajuan Judul : {{ $jadwal->nama_jadwal }}</legend>
+            <div class="form-group">
+                <label>Tgl Awal</label>
+                <input name="tgl_awal_upload" id="tgl_awal_upload" type="date" value="{{$jadwal->tanggal_awal}}" class="form-control" disabled>
+                
+              </div>
+
+              <div class="form-group">
+                <label>Tgl Akhir</label>
+                <input name="tgl_akhir_upload" id="tgl_akhir_upload" type="date" value="{{$jadwal->tanggal_akhir}}" class="form-control" disabled>
+                <span class="text-danger error" style="font-size: 12px;" id="tgl_akhir_upload_alert"></span>
+              </div>
+        </fieldset>
         <form id="form">
             <div class="row">
                 <?php
